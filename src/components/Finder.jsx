e1174f4a8feb5e03edafa5211545feb19dd449eb
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-13 16:36:45 +0100

    github user fetch api frontend mentor challenge

--- src/components/Finder.jsx
+++ src/components/Finder.jsx
@@ -5,7 +5,7 @@ import { FaMagnifyingGlass } from "react-icons/fa6";
 const Finder = () => {
   return (
     <>
-      <main className="max-w-[50%] flex justify-center flex-col items-center mx-auto h-screen">
+      <main className="max-w-[50%] flex justify-center flex-col items-center mx-auto h-screen px-3">
         <nav className="flex justify-between items-center w-full my-4">
           <h1 className="text-3xl font-bold text-gray-80>">FindDev</h1>
           <div className="flex items-center justify-between gap-3 text-xl">
@@ -13,17 +13,17 @@ const Finder = () => {
             <MdLightMode className="text-3xl" />
           </div>
         </nav>
-        <nav className="flex justify-between items-center w-full my-4 bg-[#1F2A48] px-10 py-4 shadow-lg rounded-full">
+        <nav className="flex justify-between items-center w-full my-4 bg-[#1F2A48] px-10 py-4 shadow-lg rounded-2xl">
           <div className="flex items-center gap-4">
-            <FaMagnifyingGlass className=" text-xl" />
+            <FaMagnifyingGlass className=" text-xl text-[#0079FE]" />
             <input
               type="text"
               placeholder="Search GitHub Username..."
-              className=" text-start px-4 w-full"
+              className=" text-start px-4 w-[400px] outline-none border-none bg-[#1F2A48]"
             />
           </div>
           <div className="flex items-center justify-between gap-3 text-xl">
-            <h1>Light</h1>
+            <button className="bg-[#0079FE]">Search</button>
           </div>
         </nav>
       </main>
