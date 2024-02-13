759cd88305bda0c08762d02f08c7ff9a7c262705
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-13 16:11:24 +0100

    github user fetch api frontend mentor challenge

--- src/components/Finder.jsx
+++ src/components/Finder.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { MdLightMode } from "react-icons/md";
 
 const Finder = () => {
   return (
@@ -6,7 +7,10 @@ const Finder = () => {
       <main className="max-w-[50%] flex justify-center flex-col items-center mx-auto h-screen">
         <nav className="flex justify-between items-center w-full my-4">
           <h1 className="text-3xl font-bold text-gray-80>">FindDev</h1>
-          
+          <div className="flex items-center justify-between gap-3 text-xl">
+            <h1>Light</h1>
+            <MdLightMode />
+          </div>
         </nav>
       </main>
     </>
