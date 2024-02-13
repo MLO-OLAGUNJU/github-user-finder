c0d3c723fa51703f2f04203d4794688b4fd08111
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-13 16:21:26 +0100

    github user fetch api frontend mentor challenge

--- src/components/Finder.jsx
+++ src/components/Finder.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { MdLightMode } from "react-icons/md";
+import { FaMagnifyingGlass } from "react-icons/fa6";
 
 const Finder = () => {
   return (
@@ -9,7 +10,14 @@ const Finder = () => {
           <h1 className="text-3xl font-bold text-gray-80>">FindDev</h1>
           <div className="flex items-center justify-between gap-3 text-xl">
             <h1>Light</h1>
-            <MdLightMode />
+            <MdLightMode className="text-3xl" />
+          </div>
+        </nav>
+        <nav className="flex justify-between items-center w-full my-4 bg-[#1F2A48] px-10 py-4 shadow-lg rounded-3xl">
+          <FaMagnifyingGlass className="text-3xl" />
+          <h1 className="text-3xl font-bold text-gray-80>">FindDev</h1>
+          <div className="flex items-center justify-between gap-3 text-xl">
+            <h1>Light</h1>
           </div>
         </nav>
       </main>
